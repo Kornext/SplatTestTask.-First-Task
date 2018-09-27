55943912d6c07caab74b2dc068967c150ad3f66f
Kornext <kornext@gmail.com>
2018-09-27 18:01:16 +0300

    Upd: Add create tree on form

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,19 +4,24 @@ import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.input.MouseEvent;
 import javafx.event.*;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.stage.DirectoryChooser;
+import sun.reflect.generics.tree.Tree;
 
-import java.beans.EventHandler;
-import java.io.File;
-import java.io.IOException;
+import javax.swing.tree.TreePath;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.TreeMap;
+import java.util.TreeSet;
 
 public class Controller {
 
     List<File> listResult = new ArrayList<>();
+    String searchingText;
+    String expansion;
 
     @FXML
     private Button button;
@@ -24,34 +29,70 @@ public class Controller {
     @FXML
     private TextField textField;
 
-    //searching text all be okay believe on that
+    @FXML
+    private TreeView<String> treeView;
+
+    //all be okay believe on that
 
     @FXML
     public void click(ActionEvent actionEvent) throws IOException {
-//        SecondForm secondForm = new SecondForm();
-//        secondForm.display();
         SecondForm.display();
-        DirectoryChooser directoryChooser = new DirectoryChooser();
-        Node source = (Node) actionEvent.getSource();
-        List<File> listResultSerch = null;
-        File selectedDirectory = directoryChooser.showDialog(source.getScene().getWindow());
-        if (selectedDirectory != null) {
-            if(textField.getText()!= null) {
-                listResultSerch = processFilesFromFolder(selectedDirectory, textField.getText());
+        if (SecondForm.expansion != null && SecondForm.searchingText != null) {
+            expansion = SecondForm.expansion;
+            searchingText = SecondForm.searchingText;
+            DirectoryChooser directoryChooser = new DirectoryChooser();
+            Node source = (Node) actionEvent.getSource();
+            List<File> listResultSerch = null;
+            File selectedDirectory = directoryChooser.showDialog(source.getScene().getWindow());
+            System.out.println(selectedDirectory.getPath());
+            if (selectedDirectory != null) {
+                listResultSerch = processFilesFromFolder(selectedDirectory, expansion);
             }
-            for(int i=0; i<listResultSerch.size(); i++) {
+
+
+            //Вывод
+//            for (int i = 0; i < listResultSerch.size(); i++) {
+//                System.out.println(listResultSerch.get(i).toString());
+//            }
+
+            System.out.println("******************************************");
+            for (int i = 0; i < listResultSerch.size(); i++) {
+                if (serchOnFile(searchingText, listResultSerch.get(i)) == false) {
+                    listResultSerch.remove(i);
+                }
+            }
+
+            //Вывод
+            for (int i = 0; i < listResultSerch.size(); i++) {
                 System.out.println(listResultSerch.get(i).toString());
             }
+            createTree(listResultSerch);
         }
+
+//            //Тестовое дерево
+//            TreeItem<String> rootNode = new TreeItem<>("root");
+//            TreeItem<String> node1 = new TreeItem<>("111");
+//            TreeItem<String> node2 = new TreeItem<>("222");
+//            TreeItem<String> node3 = new TreeItem<>("333");
+//            TreeItem<String> node4 = new TreeItem<>("444");
+//            TreeItem<String> node12 = new TreeItem<>("112");
+//            TreeItem<String> node13 = new TreeItem<>("113");
+//            TreeItem<String> node14 = new TreeItem<>("114");
+//            rootNode.getChildren().addAll(node1, node2, node3, node4);
+//            node1.getChildren().addAll(node12, node13, node14);
+//            treeView.setRoot(rootNode);
+//            System.out.println("**************************");
+//            List<TreeItem<String>> list = rootNode.getChildren();
+//            for(TreeItem<String> in : list) {
+//                System.out.println(in.getValue());
+//            }
     }
 
     //В рекурсии передавать и listResult
-    private List<File> processFilesFromFolder(File folder, String expansion)
-    {
+    private List<File> processFilesFromFolder(File folder, String expansion) {
         File[] folderEntries = folder.listFiles();
-        for (File entry : folderEntries)
-        {
-            if(entry.getPath().contains(expansion)) {
+        for (File entry : folderEntries) {
+            if (entry.getPath().contains(expansion)) {
                 listResult.add(entry);
             }
             if (entry.isDirectory()) {
@@ -60,8 +101,50 @@ public class Controller {
         }
         return listResult;
     }
-}
-/*
-Теперь у меня есть все файлы log. Необходимо выполнить поиск текст в них
 
- */
+    private boolean serchOnFile(String text, File file) throws IOException {
+        boolean find = false;
+        BufferedReader in = new BufferedReader(new FileReader(file));
+        String line = null;
+        while ((line = in.readLine()) != null) {
+            if (line.contains(text)) {
+                find = true;
+                break;
+            }
+        }
+        in.close();
+        return find;
+    }
+
+    private void createTree(List<File> listResultSerch) {
+        TreeItem<String> rootNode = null;
+        for (int i = 0; i < listResultSerch.size(); i++) {
+            int slash = 0;
+            String path = listResultSerch.get(i).getPath();
+            String[] words = path.split("\\\\");
+            if (i == 0) {
+                rootNode = new TreeItem<>(words[0]);
+            }
+            TreeItem<String> prefNode = rootNode;
+            for (int j = 1; j < words.length; j++) {
+                boolean flag = false;
+                List<TreeItem<String>> listChildPrefNode = prefNode.getChildren();
+                for (TreeItem<String> in : listChildPrefNode) {
+                    if (in.getValue().equals(words[j])) {
+                        prefNode = in;
+                        flag = true;
+                        break;
+                    }
+                }
+                if (flag == false) {
+                    TreeItem<String> currentNode = new TreeItem<>(words[j]);
+                    prefNode.getChildren().addAll(currentNode);
+                    prefNode = currentNode;
+                }
+            }
+        }
+        if (rootNode != null) {
+            treeView.setRoot(rootNode);
+        }
+    }
+}
--- src/sample/SecondForm.java
+++ src/sample/SecondForm.java
@@ -12,7 +12,11 @@ import java.io.IOException;
 
 public class SecondForm {
 
+    static String searchingText = null;
+    static String expansion = null;
+
     public static void display() throws IOException {
+        Stage window = new Stage();
         Button btnOk = new Button("OK");
         Button btnCancel = new Button("Cancel");
         TextArea textArea = new TextArea();
@@ -20,24 +24,28 @@ public class SecondForm {
         Label expansionLabel = new Label("Расширение");
         TextField textField = new TextField(".log");
 
+        window.setTitle("Введите текст для поиска");
         btnCancel.setLayoutX(292);
         btnCancel.setLayoutY(203);
-        btnCancel.setPrefSize(59,25);
+        btnCancel.setPrefSize(59, 25);
         btnCancel.setMnemonicParsing(false);
         btnOk.setLayoutX(16);
         btnOk.setLayoutY(203);
         btnOk.setMnemonicParsing(false);
-        btnOk.setPrefSize(59,25);
+        btnOk.setPrefSize(59, 25);
         textArea.setLayoutX(14);
         textArea.setLayoutY(44);
         textArea.setPrefSize(336, 135);
+        textArea.setWrapText(true);
         captionLable.setLayoutX(112);
         captionLable.setLayoutY(14);
         expansionLabel.setLayoutX(103);
         expansionLabel.setLayoutY(207);
         textField.setLayoutX(183);
         textField.setLayoutY(204);
-        textField.setPrefSize(68,25);
+        textField.setPrefSize(68, 25);
+
+        textArea.setText("all be okay believe on that");
 
         AnchorPane rootAnchorPane = new AnchorPane();
         rootAnchorPane.setBottomAnchor(btnCancel, 15D);
@@ -49,11 +57,17 @@ public class SecondForm {
         rootAnchorPane.setLeftAnchor(captionLable, 112D);
         rootAnchorPane.setRightAnchor(captionLable, 112D);
 
+        btnCancel.setOnAction(event -> window.close());
+        btnOk.setOnAction(event -> {
+            searchingText = textArea.getText();
+            expansion = textField.getText();
+            window.close();
+        });
+
         rootAnchorPane.getChildren().addAll(btnCancel, btnOk, expansionLabel,
                 textArea, captionLable, textField);
         rootAnchorPane.setPrefSize(365, 244);
         Scene scene = new Scene(rootAnchorPane);
-        Stage window = new Stage();
         window.initModality(Modality.APPLICATION_MODAL);
         window.setScene(scene);
         window.showAndWait();
